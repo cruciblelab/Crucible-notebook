005c3f5e781fad533ba96614b3cc6fec3ae76765
Ketipc <kettipcimm@gmail.com>
2026-07-25 09:23:06 +0300

    Fix

--- app/src/main/java/com/cruciblelab/trafficlogger/ui/HomeScreen.kt
+++ app/src/main/java/com/cruciblelab/trafficlogger/ui/HomeScreen.kt
@@ -41,6 +41,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -50,6 +52,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import com.cruciblelab.trafficlogger.HomeSummary
+import com.cruciblelab.trafficlogger.ObservedOtherDomain
 import com.cruciblelab.trafficlogger.TopAppUsage
 import com.cruciblelab.trafficlogger.ui.theme.AccentAmber
 import com.cruciblelab.trafficlogger.ui.theme.AccentCoral
